7fabbfe2641a552c8d36701949eb47650884636f
Ryan Sargeson <ryanteachviet@gmail.com>
2021-09-12 11:57:37 +0000

    finished the pricing tables with the styling and javascript, need to be responsive

--- assets/scripts.js
+++ assets/scripts.js
@@ -1,5 +1,23 @@
-const selectElement = (element) => document.querySelector(element);
+//all selectors 
 
+//selector for the navbar 
+const selectElement = (element) => document.querySelector(element);
+//sector for the pricing on home page (toggle switch)
+const toggleSwitch = document.querySelector(".toggle-switch");
+
+
+//on click listener for the navbar toggle
 selectElement('.menu-icons').addEventListener('click', () => {
     selectElement('nav').classList.toggle('active');
 })
+
+
+//toggle switch function. removes and adds pricing main if toggle switch is checked
+toggleSwitch.addEventListener("change",() =>{
+  if(toggleSwitch.checked){
+      document.querySelector(".pricing-main").classList.add("active");
+  }
+  else{
+      document.querySelector(".pricing-main").classList.remove("active");
+  }
+});
