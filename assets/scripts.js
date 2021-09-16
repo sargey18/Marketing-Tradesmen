63f91e0c1631ce0c3399a5650d1f9afbebdb3089
Ryan Sargeson <ryanteachviet@gmail.com>
2021-09-16 13:03:25 +0000

    added the form on the contact page but need to change the stylling of the form and have a javascript error

--- assets/scripts.js
+++ assets/scripts.js
@@ -1,4 +1,4 @@
-//all selectors 
+//all selectors ----------------------------------------------------------------
 
 //selector for the navbar ---------------------------------
 const selectElement = (element) => document.querySelector(element);
@@ -13,6 +13,13 @@ let contentDiv = document.getElementsByClassName('faq-content');
 //gets the class name icon
 let icons = document.getElementsByClassName('icon');
 
+//form selectors------------------------------------ 
+const form = document.getElementById('form');
+const password1El = document.getElementById('password1');
+const password2El = document.getElementById('password2');
+const messageContainer = document.querySelector('.message-container');
+const message = document.getElementById('message');
+
 
 //for the video player------------------------------
 const video = document.querySelector('video');
@@ -25,6 +32,19 @@ const volumeBar = document.querySelector('.volume-bar');
 const currentTime = document.querySelector('.time-elapsed');
 const duration = document.querySelector('.time-duration');
 const fullscreenBtn = document.querySelector('.fullscreen');
+
+
+// selectors end ---------------------------------------------------------------------
+
+// boolean start --------------------------------------------------------------------
+
+//form boolean
+let isValid = false;
+let passwordsMatch = false;
+
+
+// boolean end ------------------------------------------------------------------------
+
 
 
 
@@ -117,8 +137,79 @@ video.addEventListener('ended', showPlayIcon);
 
 
 
-//event listeners ------------------------------------------------
+
+
+//form validation start ----------------------------------------------------------------
+
+function validateForm() {
+    // Use HTML constraint API to check form validity
+    isValid = form.checkValidity();
+    // If the form isn't valid
+    if (!isValid) {
+      // Style main message for an error
+      message.textContent = 'Please fill out all fields.';
+      message.style.color = 'red';
+      messageContainer.style.borderColor = 'red';
+      return;
+    }
+    // Check to see if both password fields match
+    if (password1El.value === password2El.value) {
+      // If they match, set value to true and borders to green
+      passwordsMatch = true;
+      password1El.style.borderColor = 'green';
+      password2El.style.borderColor = 'green';
+    } else {
+      // If they don't match, border color of input to red, change message
+      passwordsMatch = false;
+      message.textContent = 'Make sure passwords match.';
+      message.style.color = 'red';
+      messageContainer.style.borderColor = 'red';
+      password1El.style.borderColor = 'red';
+      password2El.style.borderColor = 'red';
+      return;
+    }
+    // If form is valid and passwords match
+    if (isValid && passwordsMatch) {
+      // Style main message for success
+      message.textContent = 'Successfully Registered!';
+      message.style.color = 'green';
+      messageContainer.style.borderColor = 'green';
+    }
+  }
+  
+  function storeFormData() {
+    const user = {
+      name: form.name.value,
+      phone: form.phone.value,
+      email: form.email.value,
+      website: form.website.value,
+      password: form.password.value,
+    };
+    // Do something with user data
+    console.log(user);
+  }
+  
+  function processFormData(e) {
+    e.preventDefault();
+    // Validate Form
+    validateForm();
+    // Submit Form if Valid
+    if (isValid && passwordsMatch) {
+      storeFormData();
+    }
+  }
+
+// form validation end -------------------------------------------------------------------
+
+
+
+
+
+//event listeners start ------------------------------------------------
 
 //event listeners for the video
 playBtn.addEventListener('click', togglePlay);
 video.addEventListener('click', togglePlay);
+
+// form event listeners 
+form.addEventListener('submit', processFormData);
