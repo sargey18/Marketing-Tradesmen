c76e5e2f7de225dd64f425161419e2c154db1bab
Ryan Sargeson <ryanteachviet@gmail.com>
2021-09-20 20:00:21 +0000

    added the javascript but not getting any reaction from the js, added console.logs to all the eventlisteners and get el but nothing

--- assets/video.js
+++ assets/video.js
@@ -1,6 +1,28 @@
+const video = document.getElementsByClassName('video', e => {
+    console.log("test video")
+});
+const progressRange = document.querySelector('.progress-range');
+const progressBar = document.querySelector('.progress-bar');
+const playBtn = document.getElementById('play-btn', e => {
+    console.log("test button")
+});
+const volumeIcon = document.getElementById('volume-icon');
+const volumeRange = document.querySelector('.volume-range');
+const volumeBar = document.querySelector('.volume-bar');
+const currentTime = document.querySelector('.time-elapsed');
+const duration = document.querySelector('.time-duration');
+const FullscreenBtn = document.querySelector('.fullscreen');
+
+
 // Play & Pause ----------------------------------- //
 
-
+function togglePlay() {
+    if (video.paused) {
+        video.play();
+    } else {
+        video.pause();
+    }
+}
 
 // Progress Bar ---------------------------------- //
 
@@ -15,3 +37,12 @@
 
 
 // Fullscreen ------------------------------- //
+
+
+// event listeners 
+playBtn.addEventListener('click', togglePlay, e => {
+    console.log("event listener btn")
+});
+video.addEventListener('click', togglePlay, e => {
+    console.log(e)
+});
