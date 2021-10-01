f9987399a6b6eab4a52a3048a60591d1a55e0434
Ryan Sargeson <ryanteachviet@gmail.com>
2021-10-01 11:42:33 +0000

    quick save

---
+++ assets/app.js
@@ -0,0 +1,22 @@
+function populate() {
+    if(quiz.isEnded()) {
+        //showScoores();
+    }
+} else {
+    //show answers
+    let element = document.getElementById("questions");
+    element.innerHTML = quiz.getQuestionIndex().text;
+}
+
+let questions = [
+    new Question("Test question one"),
+    new Question("Test question two"),
+    new Question("Test question three"),
+    new Question("Test question four"),
+    new Question("Test question five"),
+
+];
+
+let quiz = new Quiz(questions);
+
+populate();
