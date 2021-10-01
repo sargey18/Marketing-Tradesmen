0961e6dd37160d6bc851f14c5b01ef3901b5b9fe
Ryan Sargeson <ryanteachviet@gmail.com>
2021-10-01 22:31:22 +0000

    encountered an error that is stopping me from adding text to the buttons, something about tthe not being able to read the prop of length

--- assets/app.js
+++ assets/app.js
@@ -1,20 +1,65 @@
+function Quiz(questions) {
+    this.score = 0;
+    this.questions = questions;
+    this.questionIndex = 0;
+}
+
+Quiz.prototype.getQuestionIndex = function() {
+    return this.questions[this.questionIndex];
+}
+
+Quiz.prototype.isEnded = function() {
+    return this.questions.length === this.questionIndex;
+}
+
+Quiz.prototype.guess = function(answer) {
+    if(this.getQuestionIndex().correctAnswer(answer)) {
+        this.score++;
+    }
+
+    this.questionIndex++; //swap
+
+}
+
+
+function Question(text, choice, answer) {
+    this.text = text;
+    this.choice = choice;
+    this.answer = answer;
+}
+
+Question.prototype.correctAnswer = function(choice) {
+    return choice === this.answer;
+}
+
+
 function populate() {
     if(quiz.isEnded()) {
         //showScores();
     }
      else {
     //show answers
-    let element = document.getElementById("question");
+    let element = document.getElementById ("question");
     element.innerHTML = quiz.getQuestionIndex().text;
+
+
+    // show choice 
+    let choices = quiz.getQuestionIndex().choices;
+    for(var i = 0; i < choices.length; i++) {
+        let element = document.getElementById("choice" + i);
+        element.innerHTML = choices[i];
+    }
 }
+
+
 }
 
 let questions = [
-    new Question("Test question one"),
-    new Question("Test question two"),
-    new Question("Test question three"),
-    new Question("Test question four"),
-    new Question("Test question five"),
+    new Question("Test question one", ["test", "test2", "test3", "test4"], "test4"),
+    new Question("Test question two", ["crap", "crap2", "crap3", "crap4"], "crap2"),
+    new Question("Test question three", ["shit", "shit2", "shit3", "shit4"], "shit3"),
+    new Question("Test question four", ["test", "test2", "test3", "test4"], "test4"),
+    new Question("Test question five", ["test", "test2", "test3", "test4"], "test4"),
 
 ];
 
--- assets/question.js
+++ assets/question.js
@@ -1,9 +1,9 @@
-function Question(text, choice, answer) {
-    this.text = text;
-    this.choice = choice;
-    this.answer = answer;
-}
+// function Question(text, choice, answer) {
+//     this.text = text;
+//     this.choice = choice;
+//     this.answer = answer;
+// }
 
-Question.prototype.correctAnswer = function(choice) {
-    return choice === this.answer;
-}
+// Question.prototype.correctAnswer = function(choice) {
+//     return choice === this.answer;
+// }
