d582858d1a7bb874711b03d6bd8caf84c4415934
Ryan Sargeson <ryanteachviet@gmail.com>
2021-10-05 15:35:59 +0000

    added the style and the responsive design on the end page

--- assets/end.js
+++ assets/end.js
@@ -1,7 +1,7 @@
 const GoodCustomer = () => {
     const score = window.localStorage.getItem('mostRecentScore');
     // const message = condition ? '...' : '....'
-    const message = score > 300 ? 'You are a good match!' : 'hmmm...';
+    const message = score > 300 ? 'Wow Perfect: Looks like you have everything you need' : 'Hold Up: We recommend you have some tech knowledge and have been in the Industry for a while';
 
     document.querySelector('#finalScore').innerText = message;
 
